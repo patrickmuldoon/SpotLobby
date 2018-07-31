b7a4719cae8d6e8a953a90dadf2843d7dbe2f502
Patrick <patrickmuldoon20@yahoo.com>
2018-07-30 21:40:47 -0700

    Added LoginController and LoginService, no testing for functionality at this time

---
+++ src/main/java/com/patrick/SpotLobby/Controllers/LoginController.java
@@ -0,0 +1,42 @@
+package com.patrick.SpotLobby.Controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.patrick.SpotLobby.Beans.Users;
+import com.patrick.SpotLobby.Services.LoginService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private LoginService loginService;
+
+	public void setLoginService(LoginService loginService) {
+		this.loginService = loginService;
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Users> login(@RequestBody Users user, HttpServletRequest req){
+		Users validUser = loginService.authenticate(user, req.getSession());
+		return loginService.isLoggedIn() == true ? new ResponseEntity<Users>(validUser, HttpStatus.OK) :
+			new ResponseEntity<Users>(HttpStatus.NO_CONTENT);
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public ResponseEntity<Void> logout(HttpServletRequest req){
+		HttpSession session = req.getSession();
+		loginService.logout(session);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/patrick/SpotLobby/Services/LoginService.java
@@ -0,0 +1,70 @@
+package com.patrick.SpotLobby.Services;
+
+import javax.servlet.http.HttpSession;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.patrick.SpotLobby.Beans.Users;
+import com.patrick.SpotLobby.helpers.Password;
+
+@Service
+public class LoginService {
+	
+private UsersService userService;
+	
+	private boolean isLoggedIn = false;
+	private HttpSession session;
+	
+	@Autowired
+	public void setUsersService(UsersService userService) {
+		this.userService = userService;
+	}
+	
+
+	public Users authenticate(Users user, HttpSession session){
+		if (user.getUsername() != null) {
+			String cleanUsername = Jsoup.clean(user.getUsername(), Whitelist.basic());
+			String cleanPassword = Jsoup.clean(user.getPassword(), Whitelist.basic());
+			if (cleanUsername.equals(userService.getByUsername(cleanUsername).getUsername())) {
+				Users validUser = userService.getByUsername(cleanUsername);
+				if (Password.checkPassword(cleanPassword, validUser.getPassword())) {
+					isLoggedIn = true;
+					Users sessionUser = new Users(validUser.getUserID(), validUser.getFirstName(),
+							validUser.getLastName(), validUser.getUsername(), validUser.getFollowers());
+					session.setAttribute("loggedInUser", sessionUser);
+					this.session = session;
+					return sessionUser;
+				}
+			}
+		} else {
+			return new Users();
+		}
+		return new Users();
+	}
+	
+	public void logout(HttpSession session){
+		session.invalidate();
+		isLoggedIn = false;
+	}
+
+	public boolean isLoggedIn() {
+		return isLoggedIn;
+	}
+
+	public void setLoggedIn(boolean isLoggedIn) {
+		this.isLoggedIn = isLoggedIn;
+	}
+
+	public HttpSession getSession() {
+		return session;
+	}
+
+	public void setSession(HttpSession session) {
+		this.session = session;
+	}
+	
+	
+}
